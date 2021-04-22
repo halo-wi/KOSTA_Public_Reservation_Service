2623470454c45d389b4309ec8306e046dab3c9e1
Hvvvi <soungnl@gmail.com>
2021-04-22 10:25:44 +0900

    커밋

--- src/controller/LogicTest.java
+++ src/controller/LogicTest.java
@@ -1,10 +1,12 @@
 package controller;
 
+import java.sql.SQLException;
+import model.CustomerDAO;
+
 public class LogicTest {
 
-	public static void main(String[] args) {
-		// LogicTest용 컨트롤러
-
+	public static void main(String[] args) throws SQLException {
+		CustomerDAO dao = new CustomerDAO();
+		System.out.println(dao.customer_join("sd", "df", "tgh", "sd"));
 	}
-
 }
