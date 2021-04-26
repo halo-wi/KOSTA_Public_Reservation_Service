151d63d2dbb36cfe01e7b46eadc00e3944917d44
Hoosss <hsnam1211@naver.com>
2021-04-26 19:31:58 +0900

    지역구, 날짜로 회의실 조회 회의실 예약 가능시간 없을 시 예약 불가 예약하기 클릭 시 예약 상세 페이지로 이동

--- src/controller/SelectLocationServlet.java
+++ src/controller/SelectLocationServlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -10,28 +11,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 import model.RoomDAO;
 import model.RoomVO;
 
 /**
- * Servlet implementation class SearchDetailServlet
+ * Servlet implementation class SelectSearch
  */
-@WebServlet("/Search/searchDetail")
-public class SearchDetailServlet extends HttpServlet {
+@WebServlet("/Search/roomlocation")
+public class SelectLocationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
+  
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String val= request.getParameter("roomid");
-		int room_id=Integer.parseInt(val);
-		RoomDAO dao=new RoomDAO();
-		RoomVO list=dao.selectByRoomId(room_id);
-		request.setAttribute("mapx", request.getParameter("mapx"));
-		request.setAttribute("mapy", request.getParameter("mapy"));
-		request.setAttribute("adress", request.getParameter("adress"));
-		RequestDispatcher rd=request.getRequestDispatcher("../Room/roomdetail.jsp");
+		RoomDAO roomDAO = new RoomDAO();
+		
+		List<String> room_location_list = roomDAO.selectForSearch(); // 지역구 리스트를 가져옴
+	
+		request.setAttribute("room_location_list", room_location_list);
+		RequestDispatcher rd = request.getRequestDispatcher("searchall.jsp"); // 가져온 지역구 리스트를 searchall.jsp에 전달
 		rd.forward(request, response);
+
 	}
-
-
 }
