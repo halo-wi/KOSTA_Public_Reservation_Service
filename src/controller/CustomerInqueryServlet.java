824e3eecf3e46ef8768157c38359e7299884babf
IMSEHYEOK <lsh2757@naver.com>
2021-05-03 14:09:28 +0900

    customerDAO, customerInquery, UpdateServlet

--- src/controller/CustomerInqueryServlet.java
+++ src/controller/CustomerInqueryServlet.java
@@ -1,8 +1,6 @@
 package controller;
 
-import java.beans.Statement;
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
@@ -11,43 +9,37 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
 
 import model.CustomerDAO;
 import model.CustomerVO;
 
-@WebServlet("/Mypage/Update")
-public class UpdateServlet extends HttpServlet {
+@WebServlet("/Mypage/Inquery")
+public class CustomerInqueryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		CustomerVO customerVO = new CustomerVO();
+		CustomerDAO customerDAO = new CustomerDAO();
+
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=UTF-8");
-		String id = request.getParameter("customer_id");
-		String pw = request.getParameter("customer_pw");
+		String customer_id = request.getParameter("customer_id");
+		String customer_pw = request.getParameter("customer_pw");
 		String email = request.getParameter("email");
 		String customer_name = request.getParameter("customer_name");
 		String customer_phone = request.getParameter("customer_phone");
 
-		CustomerDAO dao = new CustomerDAO();
+		
 
-		if (id != null) {
-			try {
-				int vo = dao.customer_modify(id, customer_name, pw, customer_phone, email);
-			
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			RequestDispatcher rd = request.getRequestDispatcher("mypage.jsp");
-			rd.forward(request, response);
-		} else {
-			RequestDispatcher rd = request.getRequestDispatcher("../join/Join.html");
-			rd.forward(request, response);
-		}
+		System.out.println(customer_id);
+		System.out.println(customer_pw);
+		System.out.println(email);
+		System.out.println(customer_name);
+		System.out.println(customer_phone);
 
+		
 	}
-
 }
